afc2a61656556d4ce00db6e4440d97e1888b4dd2
Adam PAPAI <wooh@wooh.hu>
2021-12-13 11:09:31 +0900

    Use RSA for signature algo

--- main.go
+++ main.go
@@ -29,10 +29,12 @@ func main() {
 	orgFlag := flag.String("org", "", "")
 	emailFlag := flag.String("email", "", "")
 	outFlag := flag.String("out", "out.csr", "")
-	orgUnitFlag := flag.String("org-unit","", "")
-	countryFlag := flag.String("country","US", "")
-	provinceFlag := flag.String("province","California", "")
-	localityFlag := flag.String("locality","San Francisco", "")
+	orgUnitFlag := flag.String("org-unit", "", "")
+	countryFlag := flag.String("country", "US", "")
+	provinceFlag := flag.String("province", "California", "")
+	localityFlag := flag.String("locality", "San Francisco", "")
+	streetFlag := flag.String("street", "", "")
+	postalCodeFlag := flag.String("postalcode", "", "")
 
 	flag.Parse()
 
@@ -53,11 +55,13 @@ func main() {
 
 	subj := pkix.Name{
 		CommonName:         *commonNameFlag,
+		Country:            []string{*countryFlag},
+		Locality:           []string{*localityFlag},
 		Organization:       []string{*orgFlag},
 		OrganizationalUnit: []string{*orgUnitFlag},
-		Country:            []string{*countryFlag},
+		PostalCode:         []string{*postalCodeFlag},
 		Province:           []string{*provinceFlag},
-		Locality:           []string{*localityFlag},
+		StreetAddress:      []string{*streetFlag},
 	}
 
 	rawSubj := subj.ToRDNSequence()
@@ -81,7 +85,7 @@ func main() {
 	// TODO Make this a flag or read from s.PublicKey?
 	//      https://cloud.google.com/kms/docs/algorithms
 	//      https://cloud.google.com/kms/docs/reference/rest/v1/projects.locations.keyRings.cryptoKeys#CryptoKeyVersionTemplate
-	template.SignatureAlgorithm = x509.ECDSAWithSHA256 // x509.SHA256WithRSAPSS
+	template.SignatureAlgorithm = x509.SHA256WithRSA // x509.SHA256WithRSAPSS
 
 	f, err := os.Create(*outFlag)
 	if err != nil {
